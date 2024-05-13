fca26b526f21cba37e092e16a613bb3d8f360fa3
17alma41 <17alma41@gmail.com>
2024-05-13 12:57:47 +0200

    feat: cameraScript

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] float smoothing;
+    [SerializeField] float lookAheadSmooth;
+    [SerializeField] float lookAheadDistance;
+    [SerializeField] float minDistanceToTarget;
+    [SerializeField] Vector3 offset = new Vector3(0, 0, 0);
+
+    Vector3 currentVelocity = Vector3.zero;
+    Vector3 lookAhead;
+    Vector3 lookAheadVelocity = Vector3.zero;
+    Vector3 directionToTarget;
+
+
+    void Start()
+    {
+        transform.position = target.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(
+            transform.localPosition,
+            target.position - target.forward * offset.x + target.up * offset.y,
+            ref currentVelocity,
+            smoothing);
+
+        directionToTarget = target.position - transform.position;
+        if (directionToTarget.magnitude < minDistanceToTarget)
+            transform.position = target.position - directionToTarget.normalized * minDistanceToTarget;
+
+        transform.LookAt(target.position);
+
+        lookAhead = Vector3.SmoothDamp(
+            lookAhead,
+            target.forward * lookAheadDistance,
+            ref lookAheadVelocity,
+            lookAheadSmooth
+            );
+
+        transform.LookAt(target.position + lookAhead);
+
+    }
+}
